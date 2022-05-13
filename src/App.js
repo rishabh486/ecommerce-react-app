d9716df7d9207bba44b2b79df3e702e2394fe3c5
rishabh486 <76812565+rishabh486@users.noreply.github.com>
2022-05-14 00:55:30 +0530

    cart-page added and routing added for cart-page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import ProductPages from "./Pages/Product-Page/ProductPage";
-
+import CartPage from "./Pages/Cart-Page/CartPage";
+import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <ProductPages />
+      <Routes>
+        <Route path="/" element={<ProductPages />} />
+        <Route path="/cart-page" element={<CartPage />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/Cart-Page/CartPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../../Components/navbar/navbar";
+import Cart from "../../Components/cart/cart";
+
+function CartPage() {
+  return (
+    <div>
+      <Navbar />
+      <Cart />
+    </div>
+  );
+}
+
+export default CartPage;
