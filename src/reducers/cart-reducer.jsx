f30e7e436aaefa29942cdd719de68c095cdbf7e6
rishabh486 <76812565+rishabh486@users.noreply.github.com>
2022-05-19 00:34:49 +0530

    signup form added

--- src/reducers/cart-reducer.jsx
+++ src/reducers/cart-reducer.jsx
@@ -12,7 +12,7 @@ export function cartReducer(state, action) {
         return {
           ...state,
           cartItems: state.cartItems - 1,
-          totalPrice: state.totalPrice - action.payload.price,
+         
         };
         case "ADD_TO_WISHLIST":
           return {
@@ -31,3 +31,38 @@ export function cartReducer(state, action) {
         return state;
     }
   }
+  export const AddToCart = (state, items) => {
+    (async (product) => {
+      try {
+        const response = await axios.post(
+          "/api/user/cart",
+          { items },
+          {
+            headers: {
+              authorization: localStorage.getItem("token"),
+            },
+          }
+        );
+      } catch (error) {
+        console.log(error);
+      }
+    })(product);
+    return { ...state, cart: [...state.cart, { ...items, quantity: 1 }] };
+  };
+  export const RemoveFromCart = (state, id) => {
+    (async (id) => {
+      try {
+        const response = await axios.delete(`/api/user/cart/:${id}`, {
+          headers: {
+            authorization: localStorage.getItem("token"),
+          },
+        });
+        notificationSuccess("REMOVED FROM CART");
+      } catch (error) {
+        console.log(error);
+        notificationError("GET FAILED");
+      }
+    })(id);
+    const newCart = state.cart.filter((product) => product._id !== id);
+    return { ...state, cart: [...newCart] };
+  };
